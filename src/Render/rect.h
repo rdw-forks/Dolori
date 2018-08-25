3ed9475d4980c220e3669f7fabecd49a422d226a
LinkZ <wanthost@gmail.com>
2018-08-25 02:56:15 +0200

    Fix windows build

--- src/Render/rect.h
+++ src/Render/rect.h
@@ -1,6 +1,7 @@
 #ifndef DOLORI_RENDER_RECT_H_
 #define DOLORI_RENDER_RECT_H_
 
+#ifndef _WIN32
 #include <stdint.h>
 
 typedef struct _RECT {
@@ -9,5 +10,8 @@ typedef struct _RECT {
   uint32_t right;
   uint32_t bottom;
 } RECT;
+#else
+#include <windows.h>
+#endif  // _WIN32
 
 #endif  // DOLORI_RENDER_RECT_H_
